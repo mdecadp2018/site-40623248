1afeebb48410bccfa175bf1c3c4f6f7ba539e491
40623248 <40623248@gm.nfu.edu.tw>
2018-10-26 15:32:54 +0800

    CMS wk5

--- flaskapp.py
+++ flaskapp.py
@@ -2001,9 +2001,7 @@ def syntaxhighlight():
 <script type="text/javascript" src="/static/gearUtils-05.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython_stdlib.js"></script>
-<style>
-img {border: 3px solid black;}
-</style>
+
 '''
 
 
@@ -2049,9 +2047,7 @@ init_mathjax();
 <script type="text/javascript" src="./../static/gearUtils-05.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython_stdlib.js"></script>
-<style><style>
-img {border: 3px solid black;}
-</style>
+
 '''
 
 
